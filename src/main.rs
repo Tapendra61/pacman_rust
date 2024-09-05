6c4d0770f22782958fcdbaee7e0bb7df53381e64
Tapendra Shahi <tapendrashahi61@gmail.com>
2024-09-05 21:46:26 +0545

    Implemented basic gameplay loop

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use macroquad::prelude::*;
 
 #[macroquad::main("Hello, world!")]
 async fn main() {
-    println!("Hello, world!");
+    let background_color = Color::new(0.45, 0.45, 0.45, 1.0);
+    clear_background(background_color);
+    loop {
+        
+        next_frame().await;
+    }
 }
