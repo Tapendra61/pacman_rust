217c1698b019b90424dd2b3b12bd526c012e945b
Tapendra Shahi <tapendrashahi61@gmail.com>
2024-09-06 15:22:28 +0545

    implemented basic player

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use macroquad::prelude::*;
 
+pub mod player;
+
 #[macroquad::main("Hello, world!")]
 async fn main() {
     let background_color = Color::new(0.45, 0.45, 0.45, 1.0);
     clear_background(background_color);
     loop {
-        
         next_frame().await;
     }
 }
